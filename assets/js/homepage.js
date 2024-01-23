1d1965264a0864a0a2f518540ea7ff03da46a484
Souhila Boukari <souhila_boukari@shi.com>
2024-01-23 09:16:05 -0500

    create a new javascript file to request Data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,6 @@
 var getUserRepos = function(){
-    fetch("https://api.github.com/users/octocat/repos");
+    var response = fetch("https://api.github.com/users/octocat/repos");
+    console.log(response);
 };
 
 getUserRepos();
